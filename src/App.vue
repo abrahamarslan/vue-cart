ccf70a3dddb5938716f689fdbb0a15234ca56e11
abrahamarslan <arslan@abrahamarslan.com>
2020-08-25 19:54:43 +0530

    #2 - Theme Setup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+        <mdb-btn color="primary">Primary</mdb-btn>
+
+        <!-- Default button -->
+        <mdb-btn color="default">Default</mdb-btn>
+
+        <!-- Secondary button -->
+        <mdb-btn color="secondary">Secondary</mdb-btn>
+
+        <!-- Indicates a successful or positive action -->
+        <mdb-btn color="success">Success</mdb-btn>
+
+        <!-- Contextual button for informational alert messages -->
+        <mdb-btn color="info">Info</mdb-btn>
+
+        <!-- Indicates caution should be taken with this action -->
+        <mdb-btn color="warning">Warning</mdb-btn>
+
+        <!-- Indicates a dangerous or potentially negative action -->
+        <mdb-btn color="danger">Danger</mdb-btn>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mdbContainer, mdbRow, mdbCol, mdbBtn } from "mdbvue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    mdbBtn
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import * as mdbvue from 'mdbvue'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 
 Vue.config.productionTip = false
 

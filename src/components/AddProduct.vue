99be42781844b92c51d82bfa81812ac1a88ea9fc
abrahamarslan <arslan@abrahamarslan.com>
2020-08-25 23:35:18 +0530

    #4 - Add product form

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,7 +1,6 @@
 <template>
  <mdb-container>
     <mdb-row>
-        <ValidationObserver v-slot="{ invalid }">
             <form @submit.prevent="onSubmit">
                 <mdb-col size="12" class="text-center mb-5">
                     <mdb-btn @click.native="showModal = true" type="button" color="info">Add New Product</mdb-btn>
@@ -10,36 +9,45 @@
                         <h4 class="title"><span class="fa fas fa-pencil-alt" /> Add New Product</h4>
                     </mdb-modal-header>
                     <mdb-modal-body class="grey-text">
-                        <ValidationProvider name="form.name" rules="required" v-slot="{ errors }">
-                            <mdb-input size="sm" v-model="form.name" name="name" label=" Product name" icon="file-signature" group type="text" validate error="wrong" success="right" trim/>
-                            <span>{{ errors[0] }}</span>
-                        </ValidationProvider>
+                        <mdb-input v-validate="{required: true, min: 3}" size="sm" v-model="form.name" name="name" label=" Product name" icon="file-signature" group type="text" validate error="wrong" success="right" trim/>                                                    
+                        <small class="form-text text-muted red-text" v-show="submitted">
+                            {{ errors.first('name') }}
+                        </small>
 
-                        <mdb-input size="sm" v-model="form.price" name="price" label="  Product price" icon="money-bill" group type="text" validate error="wrong" success="right" trim/>
+                        <mdb-input size="sm" v-validate="{required: true, numeric: true}" v-model="form.price" name="price" label="  Product price" icon="money-bill" group type="text" validate error="wrong" success="right" trim/>
+                        <small class="form-text text-muted red-text" v-show="submitted">
+                            {{ errors.first('price') }}
+                        </small>
                         <mdb-input size="sm" v-model="form.brand" name="brand" label=" Brand" icon="barcode" group type="text" validate error="wrong" success="right" trim/>
+                        <small class="form-text text-muted red-text" v-show="submitted">
+                            {{ errors.first('brand') }}
+                        </small>
                         <div>
                             <label>Inventory</label>
-                            <mdb-input type="radio" v-model="form.inventoryStatus" name="inventoryStatus" radioValue="true" id="stockTrue" label="In Stock" />
+                            <mdb-input  v-validate="{required: true}" type="radio" v-model="form.inventoryStatus" name="inventoryStatus" radioValue="true" id="stockTrue" label="In Stock" />
                             <mdb-input type="radio" v-model="form.inventoryStatus" name="inventoryStatus" radioValue="false" id="stockFalse" label="Out of stock" />
+                            <small class="form-text text-muted red-text" v-show="submitted">
+                            {{ errors.first('inventoryStatus') }}
+                        </small>
                         </div>
-                        <mdb-textarea v-model="form.description" size="sm" :rows="2" name="description" label=" Product description" icon="equals" trim/>
+                        <mdb-textarea v-validate="{required: true}" v-model="form.description" size="sm" :rows="2" name="description" label=" Product description" icon="equals" trim/>
+                        <small class="form-text text-muted red-text" v-show="submitted">
+                            {{ errors.first('description') }}
+                        </small>
                     </mdb-modal-body>
                     <mdb-modal-footer>
                         <mdb-btn type="button" color="secondary" @click.native="showModal = false">Close</mdb-btn>
-                        <mdb-btn color="primary" :disabled="invalid" type="submit" @click="addProduct()">Save changes</mdb-btn>
+                        <mdb-btn color="primary" type="submit">Save changes</mdb-btn>
                     </mdb-modal-footer>
                     </mdb-modal>
                 </mdb-col>            
             </form>
-        </ValidationObserver>
     </mdb-row>
   </mdb-container>
 </template>
 
 <script>
 import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbIcon, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter } from 'mdbvue';
-import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
-import { required, email } from 'vee-validate/dist/rules';
 export default {
     name: 'AddProduct',
     components: {
@@ -53,9 +61,7 @@ export default {
       mdbModal,
       mdbModalHeader,
       mdbModalBody,
-      mdbModalFooter,
-      ValidationProvider,
-      ValidationObserver
+      mdbModalFooter
     },
     data() {
       return {
@@ -66,15 +72,22 @@ export default {
             brand: '',
             inventoryStatus: '',
             description: ''
-        }
+        },
+        submitted: false
       };
     },
-    methods: {
-        addProduct() {
-            console.log(this.form);
-        },
-        onSubmit () {
-            alert('Form has been submitted!');
+    methods: {    
+        async onSubmit () {
+            this.submitted = true;
+            let result = await this.$validator.validate();
+            console.log(result);
+            if(result) {
+                alert('Form has been submitted!');
+            } else {
+                alert('Correct errors.');
+                return;
+            }
+            
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -4,19 +4,9 @@ import * as mdbvue from 'mdbvue'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import { ValidationObserver, ValidationProvider, extend, localize } from 'vee-validate';
-import en from 'vee-validate/dist/locale/en.json';
-import * as rules from 'vee-validate/dist/rules';
+import VeeValidate from 'vee-validate';
+Vue.use(VeeValidate);
 Vue.config.productionTip = false
-// install rules and localization
-Object.keys(rules).forEach(rule => {
-  extend(rule, rules[rule]);
-});
-
-localize('en', en);
-Vue.component('ValidationObserver', ValidationObserver);
-Vue.component('ValidationProvider', ValidationProvider);
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
